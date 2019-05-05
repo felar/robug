0ff8cd10a86430837214c810463811a6507bd8f0
Felix Günther <felix.guenther@protonmail.com>
2019-05-05 21:50:00 +0200

    Added empty ROS2 node
    Completing a task for #2

--- robug/robug_main.py
+++ robug/robug_main.py
@@ -1,11 +1,27 @@
 import rclpy
+from rclpy.node import Node
+
+
+class RobugNode(Node):
+
+    def __init__(self):
+        # Init the node, setting the name to robug_node
+        super().__init__("robug_node")
+
+        # Subscribers, publishers, etc. can/will get initialized here in the future.
 
 
 def main(args=None):
 
     rclpy.init(args=args)
-    # Code goes here. See https://github.com/ros2/examples/tree/master/rclpy
-    # for basic examples of ROS2 code in Python.
+
+    # Initialize ROS2 node
+    ros_node = RobugNode()
+
+    # "spin" the node. This halts execution until rospy gets shut down.
+    rclpy.spin(ros_node)
+
+    rclpy.shutdown()
 
 
 if __name__ == '__main__':
