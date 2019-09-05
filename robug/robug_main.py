f01e776f0ceafc999b45e2fbb0788cc062229495
Felix Günther <felix.guenther@protonmail.com>
2019-09-05 16:56:50 +0200

    Added publisher for moving (untested)

--- robug/robug_main.py
+++ robug/robug_main.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist
 
 
 class RobugNode(Node):
@@ -11,6 +12,8 @@ class RobugNode(Node):
 
         # Subscribers, publishers, etc. can/will get initialized here in the future.
         self.subscription = self.create_subscription(msg_type=LaserScan, topic='scan', callback=self.sub_call)
+        self.publisher = self.create_publisher(msg_type=Twist, topic='cmd_vel')
+
 
     def sub_call (self, msg):
         print('Nachricht empfangen!')
