6dd7ea64529ff2fe2c4e01c427aa9f4636a211da
Lara Bierbaum <lara.bierbaum@gmx.de>
2019-10-22 16:02:13 +0000

    Added observation spec

--- robug/robug_env.py
+++ robug/robug_env.py
@@ -1,10 +1,13 @@
 import numpy as np
+import rclpy
+from std_srvs.srv import Empty
 from tf_agents.specs import array_spec
 from tf_agents.environments import py_environment
 from tf_agents.trajectories import time_step
 
 
 class RobugEnv(py_environment.PyEnvironment):
+
 
     def action_spec(self):
         return array_spec.BoundedArraySpec(
@@ -17,7 +20,7 @@ class RobugEnv(py_environment.PyEnvironment):
 
     def observation_spec(self):
         return array_spec.BoundedArraySpec(
-          shape=(1,), dtype=np.int32, minimum=0, name='observation')
+          shape=(1, 360), dtype=float, name='observation')
 
     def _reset(self):
         return 0
