d808fda947cb6436d04c62b9d4c800c2e212f3ba
Lara Bierbaum <lara.bierbaum@gmx.de>
2019-10-26 14:32:27 +0000

    Implemented step-function

--- robug/robug_env.py
+++ robug/robug_env.py
@@ -1,5 +1,7 @@
 import numpy as np
 import rclpy
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
 from std_srvs.srv import Empty
 from tf_agents.specs import array_spec
 from tf_agents.environments import py_environment
@@ -14,6 +16,12 @@ class RobugEnv(py_environment.PyEnvironment):
         # Create a node and a client for the reset_simulation service that we can use later to reset the simulation
         self.reset_node = rclpy.create_node('minimal_client')
         self.reset_client = self.reset_node.create_client(Empty, 'reset_simulation')
+        self.velocity_publisher = self.create_publisher(msg_type=Twist, topic='cmd_vel')
+        self.laser_scan_subscription = self.create_subscription(msg_type=LaserScan, topic='scan', callback=self.update_observation)
+        self.latest_observation = [0.0] * 360
+
+    def update_observation(self, msg):
+        self.latest_observation = msg.ranges
 
     def action_spec(self):
         return array_spec.BoundedArraySpec(
@@ -42,12 +50,41 @@ class RobugEnv(py_environment.PyEnvironment):
         # Wait until the request is completed
         rclpy.spin_until_future_complete(self.reset_node, future)
 
-        # TODO: Return the new observation after resetting, etc.
-
-        return 0
+        return time_step.restart(self.latest_observation)
 
     def _step(self, action):
-        # TODO: Execute the action using a ROS publisher, get the sensor input from ROS, calculate rewards
-        #  and return them as a time_step with observation and reward for this step
 
-        return 0
+        if action == -1:
+            vel_turn = Twist()
+            vel_turn.linear.x = 0.1
+            vel_turn.angular.z = 0.1
+            self.velocity_publisher.publish(vel_turn)
+        elif action == 0:
+            vel_turn = Twist()
+            vel_turn.linear.x = 0.1
+            vel_turn.angular.z = 0.0
+            self.velocity_publisher.publish(vel_turn)
+        elif action == 1:
+            vel_turn = Twist()
+            vel_turn.linear.x = 0.1
+            vel_turn.angular.z = -0.1
+            self.velocity_publisher.publish(vel_turn)
+        else:
+            raise ValueError('`action` should be -1, 0 or 1.')
+
+        current_observation = self.latest_observation
+
+        in_front_of_wall = False
+        for distance in current_observation:
+            if distance == 0.0:
+                in_front_of_wall = True
+
+        if in_front_of_wall:
+            reward = -100
+        else:
+            reward = 1
+
+        if in_front_of_wall:
+            return time_step.termination(current_observation, reward)
+        else:
+            return time_step.transition(current_observation, reward)
--- robug/robug_main.py
+++ robug/robug_main.py
@@ -1,54 +1,12 @@
 import rclpy
-import tensorflow as tf
-import tf_agents as tfa
-from rclpy.node import Node
-from sensor_msgs.msg import LaserScan
-from geometry_msgs.msg import Twist
-
-
-class RobugNode(Node):
-
-    def __init__(self):
-        # Init the node, setting the name to robug_node
-        super().__init__(node_name="robug_node")
-
-        # Subscribers, publishers, etc. can/will get initialized here in the future.
-        self.subscription = self.create_subscription(msg_type=LaserScan, topic='scan', callback=self.sub_call)
-        self.publisher = self.create_publisher(msg_type=Twist, topic='cmd_vel')
-        self.switch = True
-        self.timer = self.create_timer(5, self.timer_callback)
-
-    def sub_call (self, msg):
-        vel_cmd = Twist()
-        # vel_cmd.linear.x = 0.15
-        # vel_cmd.angular.z = 0.1
-        # self.publisher.publish(vel_cmd)
-
-    def timer_callback(self):
-
-        if self.switch == True:
-            vel_straight = Twist()
-            vel_straight.linear.x = 0.1
-            vel_straight.angular.z = 0.0
-            self.publisher.publish(vel_straight)
-            self.switch = False
-        else:
-            vel_turn = Twist()
-            vel_turn.linear.x = 0.0
-            vel_turn.angular.z = 0.1
-            self.publisher.publish(vel_turn)
-            self.switch = True
+from robug_env import RobugEnv
 
 
 def main():
 
     rclpy.init()
 
-    # Initialize ROS2 node
-    ros_node = RobugNode()
-
-    # "spin" the node. This halts execution until rclpy gets shut down.
-    rclpy.spin(node=ros_node)
+    environment = RobugEnv()
 
     rclpy.shutdown()
 
